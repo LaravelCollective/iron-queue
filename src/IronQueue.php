3935de169fe74422b6f3b2b7cb311f58c2ce7be0
trideout <trideout@gmail.com>
2017-11-09 12:05:23 -0700

    Non-default queue handling
    Fix for handling queues other than the default queue.

--- src/IronQueue.php
+++ src/IronQueue.php
@@ -268,6 +268,28 @@ class IronQueue extends Queue implements QueueContract
     }
 
     /**
+     * Create a payload string from the given job and data.
+     *
+     * @param  string  $job
+     * @param  mixed   $data
+     * @return string
+     *
+     * @throws \Illuminate\Queue\InvalidPayloadException
+     */
+    protected function createPayload($job, $data = '', $queue = null)
+    {
+        $payload = json_encode($this->createPayloadArray($job, $data, $queue));
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new InvalidPayloadException(
+                'Unable to JSON encode payload. Error code: '.json_last_error()
+            );
+        }
+
+        return $payload;
+    }
+
+    /**
      * Create a payload array from the given job and data.
      *
      * @param  string  $job
